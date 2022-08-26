aee7564e4665c042b1684b32cde8e99141ca34ea
js899 <jaideepjssingh@gmail.com>
2022-08-26 10:38:06 +0530

    include a file in another file

---
+++ include statement/footer.php
@@ -0,0 +1,3 @@
+<?php
+echo "Made with love";
+?>
---
+++ include statement/index.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+    <h1>Lorem Ipsum</h1>
+    <p>Lorem Ipsum</p>
+    <p>Lorem Ipsum</p>
+    <?php
+    include "footer.php";
+    ?>
+    </body>
+</html>
