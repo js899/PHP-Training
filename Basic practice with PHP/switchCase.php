5b904cb6719fcca56a05e09c221fc2c653242c31
js899 <jaideepjssingh@gmail.com>
2022-08-31 15:26:30 +0530

    Switch Case

---
+++ Basic practice with PHP/switchCase.php
@@ -0,0 +1,17 @@
+<?php
+# include
+$input = "web";
+// $labelA = "web";
+// $labelB = "bew"; //const
+switch($input){
+    
+    case "web":
+        echo $input;
+        break;
+    case "bew":
+        echo $input;
+        break;
+    default:
+        echo "NA";
+}
+?>
